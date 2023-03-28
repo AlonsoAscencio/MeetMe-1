8a080f83df1ebc4f6c31e746d1c0c0f825a544e7
mitchelandree <117865852+mitchelandree@users.noreply.github.com>
2023-03-28 13:00:46 -0600

    Add files via upload

---
+++ MeetMe/MeetMe/MeetMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetMeApp.swift
+//  MeetMe
+//
+//  Created by MIT on 28/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MeetMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
